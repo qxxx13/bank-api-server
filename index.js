2a6088e819834e4a5cbaee708652a996eebb884d
Nikolay Nazarenko <redecv9@gmail.com>
2023-06-02 12:30:08 +0300

    Fix CORS

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const clientRouter = require("./routes/client.routes");
 const bankRouter = require("./routes/bank.routes");
 const atmRouter = require("./routes/atm.routes");
 const operationsRouter = require("./routes/operations.routes");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+const corsOptions = {
+    origin: "http://localhost:3000",
+    credentials: true,
+    optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use("/api", clientRouter);
